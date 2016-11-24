1c2a9bdbd0ed4738747679dc315164fee141d492
Noukkis <noukki99@hotmail.fr>
2016-11-24 22:09:30 +0100

    Création : 	Classe Referendum (Ne fonctionne pas encore mais on a les base) 	Classe MpCommand (Comme Command mais pour les commandes special MP) Modification : 	Classe Bot : 		Recupère les Mp et créer un MpCommand si besoin

--- src/net/lastrik/botTest/Referendum.java
+++ src/net/lastrik/botTest/Referendum.java
@@ -20,4 +20,34 @@ public class Referendum {
     private User author;
     private PrivateMessageReceivedEvent p;
     private GuildManager democracy;
+
+    public Referendum(PrivateMessageReceivedEvent p, GuildManager democracy) {
+        this.p = p;
+        this.commands = new ArrayList<>();
+        this.author = p.getAuthor();
+        this.democracy = democracy;
+    }
+
+    public void process() {
+        sayMP("New referendum created, type the commands you want to do on " + democracy.getGuild().getName());
+        listCommands();
+        listRefSpecialCommands();
+    }
+
+    private void sayMP(String sentence) {
+        author.getPrivateChannel().sendMessage(sentence);
+    }
+
+    private void sayGuild(String sentence) {
+        democracy.getGuild().getPublicChannel().sendMessage(sentence);
+    }
+
+    private void listCommands() {
+        sayMP("La liste des commandes envoyables en referendum");
+    }
+
+    private void listRefSpecialCommands() {
+        sayMP("La liste des commandes permettant de modifier le referendum (envoyer, annuler etc.)");
+    }
+
 }
